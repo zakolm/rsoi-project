45399b7041e930f7595910e3edb46bd7dc813438
ZakolM <zakolesnik.m@gmail.com>
2022-09-17 01:52:59 +0300

    v1 authorized service

---
+++ authorized/main.py
@@ -0,0 +1,67 @@
+import fastapi
+import fastapi as _fastapi
+import fastapi.security as _security
+import sqlalchemy.orm as _orm
+from typing import List
+
+import schemas as _schemas
+import services as _services
+from fastapi.middleware.cors import CORSMiddleware
+
+app = _fastapi.FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.post("/api/users")
+async def create_user(
+        user: _schemas.UserCreate,
+        db: _orm.Session = _fastapi.Depends(_services.get_db)
+):
+    db_user = await _services.get_user_by_email(user.email, db)
+    if db_user:
+        raise _fastapi.HTTPException(
+            status_code=400,
+            detail="Email already in use"
+        )
+
+    user = await _services.create_user(user, db)
+
+    return await _services.create_token(user)
+
+
+@app.post("/api/token")
+async def generate_token(
+    form_data: _security.OAuth2PasswordRequestForm = _fastapi.Depends(),
+    db: _orm.Session = _fastapi.Depends(_services.get_db),
+):
+    user = await _services.authenticate_user(
+        form_data.username,
+        form_data.password,
+        db
+    )
+
+    if not user:
+        raise _fastapi.HTTPException(
+            status_code=401,
+            detail="Invalid Credentials"
+        )
+
+    return await _services.create_token(user)
+
+
+@app.get("/api/users/me", response_model=_schemas.User)
+async def get_user(
+        user: _schemas.User = _fastapi.Depends(_services.get_current_user)
+):
+    return user
+
+
+@app.get('/api')
+async def root():
+    return {"message": "Awesome"}
---
+++ authorized/models.py
@@ -0,0 +1,18 @@
+import datetime as _dt
+
+import sqlalchemy as _sql
+import sqlalchemy.orm as _orm
+import passlib.hash as _hash
+
+import database as _database
+
+class User(_database.Base):
+    __tablename__ = "users"
+    id = _sql.Column(_sql.Integer, primary_key=True, index=True)
+    email = _sql.Column(_sql.String, unique=True, index=True)
+    hashed_password = _sql.Column(_sql.String)
+
+    leads = _orm.relationship("Lead", back_populates="owner")
+
+    def verify_password(self, password: str):
+        return _hash.bcrypt.verify(password, self.hashed_password)
